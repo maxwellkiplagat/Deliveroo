775427879e57a05d461066a8607d4d5689701dc6
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-07-20 16:12:14 +0300

    added foreign keys

--- server/models/location.py
+++ server/models/location.py
@@ -8,3 +8,15 @@ class Location(db.Model):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Foreign keys
+    parcel_id = db.Column(db.Integer, db.ForeignKey('parcel.id'), nullable=False)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'status': self.status,
+            'location': self.location_description,
+            'coordinates': {'lat': self.latitude, 'lng': self.longitude} if self.latitude else None,
+            'timestamp': self.timestamp.isoformat()
+        }
