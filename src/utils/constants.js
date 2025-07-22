2415cea1221438ccce37c524f0ef15771d40aa2e
Youngpuffy <levimuturi2@gmail.com>
2025-07-22 11:50:37 +0300

    begins constant

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const PARCEL_STATUS = {
+  PENDING: 'pending',
+  PICKED_UP: 'picked_up',
+  IN_TRANSIT: 'in_transit',
+  DELIVERED: 'delivered',
+  CANCELLED: 'cancelled',
+};
+
+export const USER_ROLES = {
+  USER: 'user',
+  ADMIN: 'admin',
+  COURIER: 'courier',
+};
+
+
+
