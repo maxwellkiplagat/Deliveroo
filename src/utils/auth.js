d24dd7ced31519c02c01688343c00362e2993957
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-07-22 09:21:10 +0300

    auth.js created to handle tokens

---
+++ src/utils/auth.js
@@ -0,0 +1,15 @@
+export const getAuthToken = () => {
+  return localStorage.getItem('token');
+};
+
+export const setAuthToken = (token) => {
+  localStorage.setItem('token', token);
+};
+
+export const removeAuthToken = () => {
+  localStorage.removeItem('token');
+};
+
+export const isAuthenticated = () => {
+  return !!getAuthToken();
+};
