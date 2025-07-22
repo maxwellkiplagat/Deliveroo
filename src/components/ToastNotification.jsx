7fa3ea216bed0a5879731b0fd076685427ce4480
Youngpuffy <levimuturi2@gmail.com>
2025-07-22 10:21:30 +0300

    updates toastNotification

--- src/components/ToastNotification.jsx
+++ src/components/ToastNotification.jsx
@@ -14,3 +14,24 @@ const colorMap = {
   error: 'bg-red-50 border-red-200 text-red-800',
   info: 'bg-blue-50 border-blue-200 text-blue-800',
 };
+
+function ToastNotification() {
+  const { notifications } = useSelector(state => state.notifications);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    notifications.forEach(notification => {
+      const timer = setTimeout(() => {
+        dispatch(removeNotification(notification.id));
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    });
+  }, [notifications, dispatch]);
+
+  return (
+    <div className="fixed top-4 right-4 z-50 space-y-2">
+      {notifications.map(notification => {
+        const Icon = iconMap[notification.type] || Info;
+        const colorClass = colorMap[notification.type] || colorMap.info;
+
