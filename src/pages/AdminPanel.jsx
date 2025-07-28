4e87cf9d72ad25b25e183122ed4a5c111c748ded
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-07-28 20:30:33 +0300

    changed some minor code

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -341,7 +341,7 @@ function AdminPanel() {
                     <tr key={parcel.id} className="hover:bg-gray-50">
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="text-sm font-medium text-gray-900">{parcel.trackingNumber}</div>
-                        <div className="text-sm text-gray-500">${parcel.price}</div>
+                        <div className="text-sm text-gray-500">ksh {parcel.price}</div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="text-sm text-gray-900">{parcel.senderName}</div>
