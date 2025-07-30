59fc3da7baf24356b3914f5418d42c3ec194b0bd
MAXWELL <maxwellkiplagat0@gmail.com>
2025-07-30 21:11:12 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -42,6 +42,11 @@ def create_app():
     @app.errorhandler(500)
     def internal_error(error):
         return jsonify({'error': 'Internal server error'}), 500
+     @app.route('/')
+    def index():
+        return {"message": "Deliveroo API is alive 🚀"}
+
+    
     
     return app
 
