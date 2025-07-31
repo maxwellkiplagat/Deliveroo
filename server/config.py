119b5b4b05cede98fa4d10ba90c34c2de3fce3c2
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-07-31 09:20:42 +0300

    fixed some

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ load_dotenv()  # Load environment variables from .env
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://username:password@localhost:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:1234@localhost:5432/deli'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-key'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
