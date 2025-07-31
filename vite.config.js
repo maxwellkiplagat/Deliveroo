44c9e999230ed357256f9bc27033e096282330bb
MAXWELL <maxwellkiplagat0@gmail.com>
2025-07-31 12:31:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: './src',
+  root: 'src',
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, './src/index.html')
+      input: path.resolve(__dirname, 'src/index.html')
     }
   },
   plugins: [react()],
