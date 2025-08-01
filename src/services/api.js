7508688551b35c610ef66930acbcf016810df055
MAXWELL <maxwellkiplagat0@gmail.com>
2025-08-01 10:41:24 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -10,4 +10,16 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// 🔥 Inject token into every request if it's in localStorage
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+
 export default api;
